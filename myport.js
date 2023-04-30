8de6adc85944eb84c71a29392c1d748d57f83ae4
SamuelOLUWASEUN <alabims10@gmail.com>
2023-04-30 10:42:18 -0700

    First Commit
    Uploaded: 30-04-2023

---
+++ myport.js
@@ -0,0 +1,20 @@
+const newsletter = document.querySelector('.newsletter-modal');
+const newsForm = document.getElementById('.news-form');
+
+window.addEventListener('load', () =>{
+    setTimeout(() =>{
+        newsletter.style.display = 'block';
+    }, 4000);
+})
+
+newsletter.addEventListener('click', (e) =>{
+    if(e.target == newsletter){
+        newsletter.style.display = 'none';
+    }
+});
+
+newsForm.addEventListener('submit', (e) =>{
+    e.preventDefault();
+
+    newsletter.style.display = 'none';
+});
